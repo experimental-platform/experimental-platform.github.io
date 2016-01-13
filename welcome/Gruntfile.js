7245a3091530d521f3891846970d89fb1897ec9c
Ömür Özkir <oemuer.oezkir@gmail.com>
2016-01-13 14:56:53 -0800

    make development more convenient with some grunt niceties

--- welcome/Gruntfile.js
+++ welcome/Gruntfile.js
@@ -22,9 +22,27 @@ module.exports = function (grunt) {
           'experimental.platform.min.css': ['node_modules/jquery-modal/jquery.modal.css', 'main.css']
         }
       }
+    },
+
+    connect: {
+      server: {
+        options: {
+          hostname:'*',
+          port: 4000, base: './' }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['main.css'],
+        tasks: ['buildcss']
+      }
     }
   });
 
+  grunt.registerTask('run', ['connect', 'watch']);
   grunt.registerTask('buildcss', ['cssmin']);
   grunt.registerTask('buildjs', ['uglify']);
   grunt.registerTask('default', ['cssmin']);
